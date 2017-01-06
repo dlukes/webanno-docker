f9409dad50a9981062c77aee5c9e5ef8686ef6c2
David Lukes <dafydd.lukes@gmail.com>
2017-01-06 16:40:35 +0100

    update shell scripts
    - add shebang to build.sh
    - rewrite run.sh for bash

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-root=${0:a:h}
+root="$(realpath "$(dirname "$0")")"
 
 docker run \
-  -v $root/mysql/conf:/etc/mysql/conf.d \
-  -v $root/mysql/data:/var/lib/mysql \
+  -v "$root"/mysql/conf:/etc/mysql/conf.d \
+  -v "$root"/mysql/data:/var/lib/mysql \
   -e MYSQL_USER=webanno -e MYSQL_DATABASE=webanno -e MYSQL_PASSWORD=t0t4llYSecreT \
   -p 18080:18080 \
   -ti ucnk/webanno
-  # -ti --entrypoint /bin/bash ucnk/webanno
